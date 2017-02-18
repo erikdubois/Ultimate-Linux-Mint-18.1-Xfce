98cbd294f24abbe6a0dfbacfb88e1be790598c04
Erik Dubois <erik.dubois@gmail.com>
2017-02-18 17:39:07 +0100

    Comment :  on Sat Feb 18 17:39:07 CET 2017

--- personal/610-install-personal-settings-keyboard-shortcuts-v3.sh
+++ personal/610-install-personal-settings-keyboard-shortcuts-v3.sh
@@ -13,6 +13,7 @@ set -e
 
 echo "copy/pasting the shortcuts"
 
+mv ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts-backup.xml 
 cp settings/keyboard-shortcuts/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 
 echo "################################################################"
